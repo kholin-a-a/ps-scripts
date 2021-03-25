1202b888ec70662518b008a3463ea0d1a4b98c5d
kholin-a-a <48410295+kholin-a-a@users.noreply.github.com>
2021-03-25 11:31:58 +0300

    main script

---
+++ Templating.ps1
@@ -0,0 +1,52 @@
+﻿function CreateBy-Template {
+    param (
+        [string]$TemplateName,
+        [string]$Name,
+        [string]$TemplatePath = "$PSScriptRoot\templates"
+    )
+
+    if (-not $TemplateName) {
+        throw "TemplateName is required"
+    }
+
+    if (-not $Name) {
+        throw "Name is required"
+    }
+
+    Get-ChildItem $TemplatePath\$TemplateName* |
+        ForEach {
+            Get-Content $_ |
+                Escape-Characters |
+                    ForEach { Invoke-Expression "`"$($_)`""} |
+                        Set-Content "$Name$($_.Extension)"
+        }
+}
+
+function Escape-Characters {
+    param (
+        [Parameter(ValueFromPipeline)]
+        $Text
+    )
+
+    Process {
+        $Text -replace '"', '`"'
+    }
+}
+
+function CebabToCamelCase {
+    param (
+        [string]$str
+    )
+
+    $regex = [Regex]"-([a-zA-Z])"
+
+    $evaluator = {
+        param (
+            $match
+        )
+
+        "$($match.Groups[1].Value)".ToUpper()
+    }
+
+    $str.Substring(0, 1).ToUpper() + $regex.Replace($str.Substring(1), $evaluator)
+}
